fd040d548d8261d2d06e21e4df830910d8af1303
Hunter <hunterweisenbach@me.com>
2025-12-07 14:45:13 -0500

    update

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -10,15 +10,27 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [ .blue, .white]), startPoint: .topLeading,
+            LinearGradient(gradient: Gradient(colors: [ .blue, .white]),
+				startPoint: .topLeading,
                 endPoint: .bottomTrailing)
-            .edgesIgnoringSafeArea(.all)
+				.edgesIgnoringSafeArea(.all)
         VStack {
                 Text("Cupertino, CA")
                     .font(.system(size: 32, weight: .medium, design: .default ))
                     .foregroundColor(Color.white)
-                    .background(Color.red)
-                    .frame(width: 200, height:200)
+					.padding()
+			VStack {
+				
+				Image(systemName: "cloud.sun.fill")
+					.renderingMode(.original)
+					.resizable()
+					.aspectRatio(contentMode: .fit)
+					.frame(width:180, height:180)
+				
+				Text("76°")
+			}
+				Spacer()
+
             
             }
         }
