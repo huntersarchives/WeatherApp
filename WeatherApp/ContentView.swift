cfe4f2803c41c33ce970b22e74e5619ac0c8f73c
Hunter <hunterweisenbach@me.com>
2025-12-07 22:23:23 -0500

    update

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -28,6 +28,24 @@ struct ContentView: View {
 					.frame(width:180, height:180)
 				
 				Text("76°")
+					.font(.system(size: 70, weight: .medium))
+					.foregroundColor(.white)
+				}
+			HStack{
+				VStack{
+					Text("TUE")
+						.font(.system(size: 16, weight: .medium, design: .default))
+						.foregroundColor(.white)
+					Image(systemName: "cloud.sun.fill")
+						.renderingMode(.original)
+						.resizable()
+						.aspectRatio(contentMode: .fit)
+						.frame(width:40, height:40)
+					Text("76°")
+						.font(.system(size:28, weight: .medium))
+						.foregroundColor(.white)
+					
+				}
 			}
 				Spacer()
 
